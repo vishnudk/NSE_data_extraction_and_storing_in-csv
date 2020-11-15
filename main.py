d0b08c1c10d3fc2120e430f37583406251b55646
vishnudk <vishnudk179@gmail.com>
2020-11-15 13:14:25 +0530

    compeleted the project

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from nsetools import Nse
 import requests as re
 import csv
+from datetime import date
 import datetime 
 import time
 def function():
    t = 1
    while 1:
-      time.sleep(t)
+      # time.sleep(t)
       data = get_nse_data()
       if data!=None:
          enter_data_to_csv(data)
-         t = 10
-      else:
-         t = 1
+         break
+      #    t = 10
+      # else:
+      #    t = 1
 
 
 def get_nse_data():
+   # print("get data function")
    # headers = {
        
    #  'User-Agent': 'Chrome/ 86.0.4240.193'
@@ -29,28 +32,73 @@ def get_nse_data():
    while 1:
       try:
          data_nifty = re.get(URL1,headers=headers).json()
+         # print("got the data ")
          break
       except:
-         break
-   try:
-      try:
-         print(list(data_nifty['records']['data'][len(data_nifty['records']['data'])-1]['CE'].values()))
-      except:
-         print(list(data_nifty['records']['data'][0]['CE'].values()))
-   except:
-      return None
-   # with open('nse_india_data.csv', 'w', newline='') as file:
-   #    writer = csv.writer(file)
-   #    writer.writerows(list([data_nifty['records']['data'][0]['PE'].keys()]))
-   return list(data_nifty['records']['data'][0])
+         pass 
+         # break
+   # for i in data_nifty['records']['data']:
+   #    print(i)
+   #    print("==============")
+   # # print("got out of the loop and now goig to prosecess the data and then to save it in the csv file")
+   # try:
+   #    try:
+   #       print(list(data_nifty['records']['data'][len(data_nifty['records']['data'])-1]['CE'].values()))
+   #    except:
+   #       print(list(data_nifty['records']['data'][0]['CE'].values()))
+   #    print("got the value")
+   # except:
+   #    return None
+   # # with open('nse_india_data.csv', 'w', newline='') as file:
+   # #    writer = csv.writer(file)
+   # #    writer.writerows(list([data_nifty['records']['data'][0]['PE'].keys()]))
+   # # return list(data_nifty['records']['data'][0])
+   return data_nifty
 
 def enter_data_to_csv(data):
-   dateandTime = datetime.datetime.now()
-   data.append(dateandTime.strftime("%X"))
-   data = list([data])
-   with open('nse_india_data.csv', 'a', newline='') as file:
-      writer = csv.writer(file)
-      writer.writerows(data)
+   time = datetime.datetime.now()
+   today = date.today()
+   date1 = today.strftime("%b-%d-%Y")
+   for i in (data['records']['data']):
+      in_val = list([i['strikePrice'],i['expiryDate']])
+      peData = [""]*19
+      ceData = [""]*19
+      try:
+         print("PE")
+         tmpdata = i['PE'].values()
+         if tmpdata!="":
+            peData = list(tmpdata)
+            print(peData)
+      except:
+         pass
+      try:
+         print("CE")
+         tmpdata = i['CE'].values()
+         if tmpdata!="":
+            ceData = list(tmpdata)
+            print(ceData)
+      except:
+         pass
+      peData.extend(ceData)
+      peData.append(date1)
+      peData.append(time.strftime("%X"))
+      in_val.extend(peData)
+      peData =  list([in_val])
+      print("full data with appended date and time")
+      print(peData)
+      with open('nse_india_data.csv', 'a', newline='') as file:
+         writer = csv.writer(file)
+         writer.writerows(peData)
+      print("=============")
+   # for i in data['records']['data']:
+   #    print(i)
+   #    print("============================================")
+   # dateandTime = datetime.datetime.now()
+   # data.append(dateandTime.strftime("%X"))
+   # data = list([data])
+   # with open('nse_india_data.csv', 'a', newline='') as file:
+   #    writer = csv.writer(file)
+   #    writer.writerows(data)
 
 
 if __name__ == "__main__": 
